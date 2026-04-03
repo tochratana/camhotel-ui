0e94e60216a5a8f5e7180ea01ec50df1cccf32da
MenglongK <menglongkeo07@gmail.com>
2026-04-03 16:49:12 +0700

    update list room UI only static

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,7 +1,5 @@
+import RoomListUser from "@/components/rooms/RoomListUser";
+
 export default function Page(){
-    return(
-        <>
-            <h2>Room page</h2>
-        </>
-    )
+    return <RoomListUser/>
 }
