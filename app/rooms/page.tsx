8f210e1856d0998c980f7119b1e11f17d41b6a42
MenglongK <menglongkeo07@gmail.com>
2026-04-03 17:14:52 +0700

    update Booking for user view static components

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,9 +1,7 @@
 import RoomListUser from "@/components/rooms/RoomListUser";
-import RoomDetailUser from "@/components/rooms/RoomDetailUser";
 
 export default function Page(){
     return (
-        <RoomDetailUser/>
-        // <RoomListUser/>
+        <RoomListUser/>
     )
 }
